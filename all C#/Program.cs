e252588a91fb0594f1cb18342dce092ec57bee89
KRISH <KRISH@DESKTOP-HVP8DN6>
2025-12-27 21:32:20 +0530

    Add project files.

---
+++ all C#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace all_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // All Data Types
+            int id = 1;
+            string name = "krish";
+            char letter = 'y';
+            bool condition = true;
+            long lnum = 2407302L;
+            decimal nnum = 2.33M;
+            float num = 3.4454F;
+            double dnum = 230974.423;
+
+            // String Conversion
+            String val1 = "1";
+            Console.WriteLine(Convert.ToInt32(val1));
+
+
+
+        }
+    }
+}
