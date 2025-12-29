655d7046d8d902d9e839e816859731a1c2b83a50
beast boy <krishtiwari00@gmail.com>
2025-12-29 15:53:20 +0530

    29-12-2025

--- all C#/Program.cs
+++ all C#/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace all_C_
 {
@@ -16,179 +17,261 @@ namespace all_C_
             //Allow Unicode characters to be displayed in console
 
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            /*
+                        // All Data Types
+                        int id = 1;
+                        string name = "krish";
+                        char letter = 'y';
+                        bool condition = true;
+                        long lnum = 2407302L;
+                        decimal nnum = 2.33M;
+                        float num = 3.4454F;
+                        double dnum = 230974.423;
 
-            // All Data Types
-            int id = 1;
-            string name = "krish";
-            char letter = 'y';
-            bool condition = true;
-            long lnum = 2407302L;
-            decimal nnum = 2.33M;
-            float num = 3.4454F;
-            double dnum = 230974.423;
+                        // String Conversion
+                        string val1 = "1";
+                        Console.WriteLine(Convert.ToInt32(val1));
 
-            // String Conversion
-            string val1 = "1";
-            Console.WriteLine(Convert.ToInt32(val1));
+                        string val2 = "y";
+                        Console.WriteLine(Convert.ToChar(val2));
 
-            string val2 = "y";
-            Console.WriteLine(Convert.ToChar(val2));
+                        string val3 = "true";
+                        Console.WriteLine(Convert.ToBoolean(val3));
 
-            string val3 = "true";
-            Console.WriteLine(Convert.ToBoolean(val3));
+                        string val4 = "239659";
+                        Console.WriteLine(Convert.ToInt64(val4));
 
-            string val4 = "239659";
-            Console.WriteLine(Convert.ToInt64(val4));
+                        string val5 = "3.44";
+                        Console.WriteLine(Convert.ToDecimal(val5));
 
-            string val5 = "3.44";
-            Console.WriteLine(Convert.ToDecimal(val5));
+                        string val6 = "96235232.32532";
+                        Console.WriteLine(Convert.ToDouble(val6));
 
-            string val6 = "96235232.32532";
-            Console.WriteLine(Convert.ToDouble(val6));
+                        string val7 = "4.547574";
+                        Console.WriteLine(Convert.ToSingle(val7));
 
-            string val7 = "4.547574";
-            Console.WriteLine(Convert.ToSingle(val7));
+                        // Operators 
+                        // +, -, *, /, %
+                        int xv = 0;
+                        Console.WriteLine(xv++); //here it will print 0 and then will change its val to 1
+                        Console.WriteLine(++xv); //here the value which is converted to 1 will increment op=2
 
-            // Operators 
-            // +, -, *, /, %
-            int xv = 0;
-            Console.WriteLine(xv++); //here it will print 0 and then will change its val to 1
-            Console.WriteLine(++xv); //here the value which is converted to 1 will increment op=2
+                        int xv1 = 5;
+                        Console.WriteLine(xv1--);//5
+                        Console.WriteLine(--xv1);//3
 
-            int xv1 = 5;
-            Console.WriteLine(xv1--);//5
-            Console.WriteLine(--xv1);//3
+                        int xv2 = 0;
+                        xv2++;
+                        Console.WriteLine(xv2);//1
 
-            int xv2 = 0;
-            xv2++;
-            Console.WriteLine(xv2);//1
+                        int xv3 = 0;
+                        ++xv3;
+                        Console.WriteLine(xv3);//1
 
-            int xv3 = 0;
-            ++xv3;
-            Console.WriteLine(xv3);//1
+                        // Operators with string
 
-            // Operators with string
+                        string op1 = "Krish ";
+                        string op2 = "Tiwari";
+                        Console.WriteLine(op1 + op2);
 
-            string op1 = "Krish ";
-            string op2 = "Tiwari";
-            Console.WriteLine(op1 + op2);
+                        // Operators with char
 
-            // Operators with char
+                        char ch1 = 'k';
+                        char ch2 = 't';
+                        Console.WriteLine(ch1 + ch2);
+                        Console.WriteLine(ch1 - ch2);
+                        Console.WriteLine(ch1 / ch2);
+                        Console.WriteLine(ch1 * ch2);// it will use unicode to operate 
+                        // and output will be in unicode values
 
-            char ch1 = 'k';
-            char ch2 = 't';
-            Console.WriteLine(ch1 + ch2);
-            Console.WriteLine(ch1 - ch2);
-            Console.WriteLine(ch1 / ch2);
-            Console.WriteLine(ch1 * ch2);// it will use unicode to operate 
-            // and output will be in unicode values
+                        // var keyword
 
-            // var keyword
+                        var vartxt = 122.33F;
+                        // U can declare anything in var just dont use it much it decreases rediability of code
 
-            var vartxt = 122.33F;
-            // U can declare anything in var just dont use it much it decreases rediability of code
+                        /* multi line comment
+                        can be written like this*/
+            /*
+                        const string password = "krish9239438";
+                        //password = "123"; // throw error const cant be changed
 
-            /* multi line comment
-            can be written like this*/
+                        // Taking Input 
 
-            const string password = "krish9239438";
-            //password = "123"; // throw error const cant be changed
+                        string sinp = Console.ReadLine(); // only for string
+                        Console.WriteLine($"youve have written {sinp}");
 
-            // Taking Input 
+                        int iinp = Convert.ToInt32(Console.ReadLine());// convert to take inp of other data types
+                        Console.WriteLine($"youve entered integer {iinp}");
 
-            string sinp = Console.ReadLine(); // only for string
-            Console.WriteLine($"youve have written {sinp}");
+                        // C# Typecasting
 
-            int iinp = Convert.ToInt32(Console.ReadLine());// convert to take inp of other data types
-            Console.WriteLine($"youve entered integer {iinp}");
+                        int anynum = 23423;
+                        string anynumtp = anynum.ToString();//no implicit or explicit for string
 
-            // C# Typecasting
-
-            int anynum = 23423;
-            string anynumtp = anynum.ToString();//no implicit or explicit for string
-
-            string s = "123";
-            int n = int.Parse(s);      // string → number
+                        string s = "123";
+                        int n = int.Parse(s);      // string → number
 
 
-            int tc1 = 2;
-            double tc2 = tc1;// declared with new data type is conv automatically (implicit)
+                        int tc1 = 2;
+                        double tc2 = tc1;// declared with new data type is conv automatically (implicit)
 
-            int tc3 = 2;
-            double tc4 = (double)tc3;// explicit
+                        int tc3 = 2;
+                        double tc4 = (double)tc3;// explicit
 
-            // if state ment
+                        // if state ment
 
-            int i = 5;
-            if (i % 2 == 0)
+                        int i = 5;
+                        if (i % 2 == 0)
+                        {
+                            Console.WriteLine("even");
+                        }
+                        else
+                        {
+                            Console.WriteLine("odd");
+                        }
+
+                        // Switch case
+
+                        Console.WriteLine("choose 1 for true and 2 for false");
+                        int swinp = Convert.ToInt32(Console.ReadLine());
+
+                        switch (swinp)
+                        {
+                            case 1: Console.WriteLine(true);
+                                break;
+                            case 2: Console.WriteLine(false);
+                                break;
+                        }
+
+                        // loops
+
+                        for (int m = 1; m <= 3; m++)
+                        {
+                            Console.WriteLine(m);
+                        }
+
+                        Console.WriteLine();
+
+                        int o = 1;
+                        while (o <= 3)
+                        {
+                            Console.WriteLine(o++);
+
+                        }
+
+                        // Number formatting
+                        double nf = (Double)10.0 / 3.0;
+                        Console.WriteLine("{0:0.00}",nf);
+
+                        //Currency
+                        double c = (double)10.0 / 3.0;
+                        Console.WriteLine(c.ToString("C",CultureInfo.CreateSpecificCulture("en-GB")));
+                        //correct way
+
+                        double newc = (double)67.0 / 5.0;
+                        Console.WriteLine(c.ToString("C",CultureInfo.CreateSpecificCulture("en-IN")));
+
+                        //Try Parse Function
+                        string numinp = Console.ReadLine();
+                        int defaultval = 0;
+                        Console.WriteLine( int.TryParse(numinp,out defaultval));
+                        //op true or flase
+                        //input only string
+
+                        // verbatium String
+                        string vers = @"afdh ///af/a/f a/fs";
+                        string vers1 = @"afdh \n \\af\a/f a/fs";
+                        Console.WriteLine(@"sajhs""sas0k");
+
+                        //Concateination
+                        String concstri = string.Concat("a","b",vers1);
+
+                        //more better concateination which increase readability of code
+                        Console.WriteLine($"you declared{vers}");
+
+
+                        // Empty string
+
+                        // you cna use it instead of ""  
+
+                        Console.WriteLine("Enter your name");
+                        string namein = Console.ReadLine();
+
+                        if (namein.Equals(string.Empty))// concern about values
+                        {
+                            Console.WriteLine($"your name is {namein}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("name is empty");
+                        }
+            */
+
+            // String is array of char[]
+
+            //string message = "C# is interesting";
+            //Console.WriteLine(message[0]); //C
+            //Console.WriteLine(message.Length);
+            //Console.WriteLine(message.Contains("C"));
+            //for (int w = 0; w < message.Length; w++)
+            //{
+
+            //    Console.Write(message[w]);
+            //    Thread.Sleep(100);
+
+            //}
+            //Console.WriteLine();
+
+            ////-------------
+
+            //bool tf = false;                 // create a boolean flag, initially false
+
+            //for (int w = 0; w < message.Length; w++)  // loop through each character in the string
+            //{
+            //    // message[w] gives the character at index w
+
+            //    if (message[w].Equals('C'))  // check if the current character is 'C'
+            //    {
+            //        tf = true;               // if 'C' is found, set flag to true
+            //    }
+            //}
+
+            //// loop has finished checking all characters
+            //// tf now tells whether 'C' was found or not
+
+            //Console.WriteLine(tf);           // prints true if 'C' was found, otherwise false
+
+            ////--------------------
+
+
+            //// is null or is empty function
+            //Console.WriteLine("Enter your name");
+            //String nam = Console.ReadLine();
+
+
+            //if (!string.IsNullOrEmpty(nam))
+            //{
+            //    Console.WriteLine(1);
+            //}
+
+            // Exercise take input print in reverse and in order
+            Console.WriteLine("Enter information");
+            string info = Console.ReadLine();
+            
+            Console.WriteLine(" your orderd info is");
+            
+            for(int i =0; i < info.Length; i++)
             {
-                Console.WriteLine("even");
+                Console.Write(info[i]);
             }
-            else
+
+            Console.WriteLine(" your reversed info is");
+            
+            for (int i= info.Length-1; i >= 0; i--)
             {
-                Console.WriteLine("odd");
+                Console.Write(info[i]);
             }
-
-            // Switch case
-
-            Console.WriteLine("choose 1 for true and 2 for false");
-            int swinp = Convert.ToInt32(Console.ReadLine());
-
-            switch (swinp)
-            {
-                case 1: Console.WriteLine(true);
-                    break;
-                case 2: Console.WriteLine(false);
-                    break;
-            }
-
-            // loops
-
-            for (int m = 1; m <= 3; m++)
-            {
-                Console.WriteLine(m);
-            }
-
-            Console.WriteLine();
-
-            int o = 1;
-            while (o <= 3)
-            {
-                Console.WriteLine(o++);
-                
-            }
-
-            // Number formatting
-            double nf = (Double)10.0 / 3.0;
-            Console.WriteLine("{0:0.00}",nf);
-
-            //Currency
-            double c = (double)10.0 / 3.0;
-            Console.WriteLine(c.ToString("C",CultureInfo.CreateSpecificCulture("en-GB")));
-            //correct way
-
-            double newc = (double)67.0 / 5.0;
-            Console.WriteLine(c.ToString("C",CultureInfo.CreateSpecificCulture("en-IN")));
-
-            //Try Parse Function
-            string numinp = Console.ReadLine();
-            int defaultval = 0;
-            Console.WriteLine( int.TryParse(numinp,out defaultval));
-            //op true or flase
-            //input only string
-
-            // verbatium String
-            string vers = @"afdh ///af/a/f a/fs";
-            string vers1 = @"afdh \n \\af\a/f a/fs";
-            Console.WriteLine(@"sajhs""sas0k");
-
-            //Concateination
-            String concstri = string.Concat("a","b",vers1);
-
-            //more better concateination which increase readability of code
-            Console.WriteLine($"you declared{vers}");
-
-        }
-        
-    }
+            
+        }         
+    }   
 }
