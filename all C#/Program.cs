1caefde478ede7ab1eb65cee2f04f74def4411ed
beast boy <krishtiwari00@gmail.com>
2026-01-05 12:52:32 +0530

    5-02-2026

--- all C#/Program.cs
+++ all C#/Program.cs
@@ -6,6 +6,10 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Xml.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Configuration;
+
 
 namespace all_C_
 {
@@ -255,23 +259,341 @@ namespace all_C_
             //}
 
             // Exercise take input print in reverse and in order
-            Console.WriteLine("Enter information");
-            string info = Console.ReadLine();
-            
-            Console.WriteLine(" your orderd info is");
-            
-            for(int i =0; i < info.Length; i++)
-            {
-                Console.Write(info[i]);
-            }
+            //Console.WriteLine("Enter information");
+            //string info = Console.ReadLine();
 
-            Console.WriteLine(" your reversed info is");
+            //Console.WriteLine(" your orderd info is");
+
+            //for(int i =0; i < info.Length; i++)
+            //{
+            //    Console.Write(info[i]);
+            //}
+
+            //Console.WriteLine(" your reversed info is");
+
+            //for (int i= info.Length-1; i >= 0; i--)
+            //{
+            //    Console.Write(info[i]);
+            //}
+
+            //----------------------------------------
+
+            // Arrays
+            //int[] arr = new int[3];
+            //arr[0] = 1;
+            //arr[1] = 3;
+            //arr[2] = 2;
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    sum = sum + arr[i];
+
+            //}
+            //Console.WriteLine(sum);
+
+            //// search max num in array
+            //int max = arr[0];
+            //int maxposition = 0;
+
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > max)
+            //    {
+            //        max = arr[i];
+            //        maxposition = i;
+            //    } 
+            //}
+            //Console.WriteLine($"max number is {max} at the position {maxposition}");
+
+            // max no in array
+            //int[] arrayy = new int[3];
+            //arrayy[0] = 3;
+            //arrayy[1] = 9;
+            //arrayy[2] = 7;
+
+
+            //int maxnum = arrayy[0];
+            //int position = 0;
+            //for (int i = 0; i < arrayy.Length; i++)
+            //{
+            //    if (arrayy[i] > maxnum)
+            //    {
+            //        maxnum = arrayy[i];
+            //        position = i;
+            //    }
+            //}
+            //Console.WriteLine($"max num in array is {maxnum} at position {position}");
+
+            //// take input in array
+            //Console.WriteLine("Enter size of array");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] arr1 = new int[n];
+            //Console.WriteLine("Enter values in array");
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    arr1[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("The array youve entered is given as follows :");
+            //Console.Write("[");
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    Console.Write($" {arr1[i]} ");
+            //}
+            //Console.Write("]");
+
+            //Console.WriteLine();
+
+            //// for each
+            // used when you want to go from every case
+            //foreach (int item in arr1)
+            //{
+            //    Console.Write($" {item} ");
+            //}
+
+            // check if its triangle
+
+            //Console.WriteLine(" Enter 3 angle of triangle");
+            //int[] angles = new int[3];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    angles[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //int sumAngle = angles[0] + angles[1] + angles[2];
+            //Console.WriteLine(sumAngle == 180 ? "valid" : "Invalid");
+
+
+            // array.sort funct
+
+            //int[] arrSort = new int[]
+            //{
+            //    1,5,6,73,36,2,74,3
+            //};
+            //Array.Sort(arrSort);
+            //foreach (var item in arrSort)
+            //{
+            //    Console.Write($" {item} "); // sorted array output
+
+            //}
+            //Console.WriteLine();
+
+            //// array.Reverse function
+            //Array.Reverse(arrSort);
+            //foreach (var item in arrSort)
+            //{
+            //    Console.Write($" {item} "); // sorted array output
+
+            //}
+
+            //Console.WriteLine();
+
+            //// type forr tab to get the reverse for loop
+            //for (int i = arrSort.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write($" {arrSort[i]} ");
+            //}
+            //Console.WriteLine();
+
+            //// clear array
+            //Array.Clear(arrSort, 0, arrSort.Length);
+            //for (int i = 0; i < arrSort.Length; i++)
+            //{
+            //    Console.Write(arrSort[i]+" ");
+            //}
+
+            //int search = 5;
+
+            //// INDEX OF
+            //Console.WriteLine( Array.IndexOf(arrSort, search, 0, arrSort.Length)); 
+            ////-1 == not found coz we cleared array its 0
+
+            //------------------------------------------
+
+            // LIST
+
+            //List<string> ListNum = new List<string>();
+
+            //bool condition = true;
+            //while (condition == true) {
+            //    Console.WriteLine(" press 1. add & 2. exit");
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //    if (x == 1 || x == 2)
+            //    {
+            //        switch (x)
+            //        {
+            //            case 1:
+            //                Console.WriteLine(" add notes");
+            //                ListNum.Add(Console.ReadLine());
+            //                condition = true;
+            //                break;
+
+            //            case 2:
+            //                Console.WriteLine("exiting");
+            //                condition = false;
+            //                break;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("wrong input");
+            //        condition = true;
+            //    }
+
+            //}foreach (var item in ListNum)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+
+            ////------------------------------------------
+
+            //// Dictioary
+
+            //Dictionary<int, string> DictList = new Dictionary<int, string>
+            //{
+            //    { 1, "krish" },
+            //    { 2, "Y"},
+            //    { 3, "Tiwari" }
+
+            //   };
+
+            ////------------------------------------------
+
+            // Exercise
+
+            /* 
+                  Create 2 lists with int data type, one for even num, one for odd
+                  loop from 0-20
+                    if no is even add to even list
+                    if no is odd add to odd list
+                print even list
+                print odd list
+
+             */
+
+            //List<int> even = new List<int>();
+            //List<int> odd = new List<int>();
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        even.Add(i);
+            //    }
+            //    else
+            //    {
+            //        odd.Add(i);
+            //    }
+            //}
+            //Console.WriteLine("even :");
+            //foreach (var item in even )
+            //{
+            //    Console.Write(item+" ");
+            //}
+            //Console.WriteLine();
             
-            for (int i= info.Length-1; i >= 0; i--)
+            //Console.WriteLine("odd :");
+            //foreach (var item in odd)
+            //{
+            //    Console.Write(item+" ");
+            //}
+
+            //Console.WriteLine();
+
+            ////
+
+            //int f = 0;
+            //int[] sarr = new int[5];
+            //for (int i = 1; i <= sarr.Length; i++)
+            //{
+            //    sarr[f] = i * 7;
+            //    f++;
+            //}
+            //foreach (var item2 in sarr)
+            //{
+            //    Console.Write(item2+" ");
+            //}
+
+            // Functions in C#
+
+            // 1. Void functions
+
+            void SwitchOnFan()
             {
-                Console.Write(info[i]);
+                Console.WriteLine("Fan is On");
             }
-            
-        }         
-    }   
+            SwitchOnFan();
+
+            // 2. Return Type Function
+
+            int AAdd(int m , int n)
+            {
+                return m + n;
+            }
+            int sum = AAdd(3 , 4);
+            Console.WriteLine(sum);
+
+            // 3. Function Parameter
+
+            void PrintName(string name)
+            {
+                Console.WriteLine("hello "+ name);
+            }
+            PrintName("c#");
+
+            // 4. Optional Parameter
+
+            void BuyCoffee(string type, int sugar = 1)
+            {
+                Console.WriteLine(type+" coffee with "+sugar+" sugar.");
+            }
+            BuyCoffee("cold");
+            BuyCoffee("cold", 2);
+
+            // named parameter
+
+            void pnam(string nam, int rollNo)
+            {
+                Console.WriteLine(nam+" "+rollNo);
+            }
+            pnam(rollNo: 7,nam: " krish");// sequence doesnt matter
+
+            // Reference parameter
+
+            void AddBonus(ref int sal)
+            {
+                sal += 5000;
+            }
+            int sala = 50000;
+            AddBonus(ref sala);
+            Console.WriteLine(sala);
+
+            //area of triangle
+
+            double AreaOfTriangle(double b,double h)
+            {
+                return 0.5 * b * h;
+            }
+            double Aot = AreaOfTriangle(5 , 10);
+            Console.WriteLine(Aot);
+
+            // sum of int array
+            int array(int[] a)
+            {
+                int summ =0;
+                foreach (var item in a)
+                {
+                    summ += item;
+                }
+                return summ;
+            }
+            int[] baa = new int[]
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+            array(baa);
+           
+        }
+    }
 }
